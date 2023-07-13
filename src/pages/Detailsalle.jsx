c42f45429bdd6029ed5108cefea90560145d24d9
Riadh Idir <riadhidir5@gmail.com>
2023-07-13 17:33:20 +0100

    ff

--- src/pages/Detailsalle.jsx
+++ src/pages/Detailsalle.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const Detailsalle = () => {
   const [salle, setSalle] = useState(null);
